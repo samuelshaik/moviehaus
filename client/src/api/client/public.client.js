b26e44363b5753b18aab5e0f97804b5dff335217
Mahammad Sami <mahammadsami@Mahammads-MacBook-Pro.local>
2026-02-01 22:39:15 +0530

    Update API base URLs for deployment

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,34 +1,6 @@
-/*import axios from "axios";
-import queryString from "query-string";
+import axios from "axios";
 
-const baseURL ="http://localhost:5002/api/v1" ;
-
-const publicClient = axios.create({
-  baseURL,
-  paramsSerializer: {
-    encode: params => queryString.stringify(params)
-  }
-});
-
-publicClient.interceptors.request.use(async config => {
-  return {
-    ...config,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
-});
-
-publicClient.interceptors.response.use((response) => {
-  if (response && response.data) return response.data;
-  return response;
-}, (err) => {
-  throw err.response.data;
-});
-
-export default publicClient;*/import axios from "axios";
-
-const baseURL = "http://localhost:5002/api/v1";
+const baseURL = "https://moviehaus.onrender.com/api/v1";
 
 const publicClient = axios.create({
   baseURL,
